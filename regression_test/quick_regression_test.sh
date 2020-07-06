6bc46e32d5c8e528b90781aaa0338366c125b15d
Richard Barnes <rbarnes@umn.edu>
2020-07-06 10:20:16 -0600

    Added regression tests

---
+++ regression_test/quick_regression_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+base_dir="${script_dir}/.."
+prog="${base_dir}/build/test_prog"
+query_input="${base_dir}/build/query_batch.fasta"
+target_input="${base_dir}/build/target_batch.fasta"
+output_dir="${script_dir}"
+${prog} -s -p -y local ${query_input} ${target_input} > "${output_dir}/new_start"
+${prog} -t -p -y local ${query_input} ${target_input} > "${output_dir}/new_tb"
+${prog}    -p -y local ${query_input} ${target_input} > "${output_dir}/new_standard"
+
+echo "DIFFS"
+diff "${output_dir}/new_start" "${output_dir}/original_start"
+diff "${output_dir}/new_tb" "${output_dir}/original_tb"
+diff "${output_dir}/new_standard" "${output_dir}/original_standard"
